69a536119dbca32b7dbd5593cae865e1d00e7cd0
Kamal Prakash Verma <kamalprakashverma@Kamals-MacBook-Air.local>
2025-05-10 21:54:49 +0530

    first commit

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,5 +5,5 @@ dotenv.config();
 const port = process.env.PORT || 8080;
 // start server
 app.listen(port,()=>{
-    console.log(`Server is running on port http://localhost:${port}`);
+    console.log(`Server is running on port http://localhost:${port} in ${process.env.NODE_ENV} mode`);
 })
